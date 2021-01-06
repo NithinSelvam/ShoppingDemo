8605412d9b2108098c3001e42435830a31f5b72d
Nithin <nithselv@gmail.com>
2021-01-06 14:18:34 +0530

    landing and header container

---
+++ src/view/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = ({ name }) => {
+    return (
+        <div>
+            {name}
+            Header
+        </div>
+    )
+}
+
+export default Header
---
+++ src/view/page/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../component/Header'
+
+export const LandingPage = () => {
+    return (
+        <div>
+            <Header name={'nithn'} />
+            welcome
+        </div>
+    )
+}
+
+export default LandingPage
