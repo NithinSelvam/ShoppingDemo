044f0a0d474533f0fdea52bc538f2e1e37b67326
Nithin <nithselv@gmail.com>
2021-01-06 21:14:11 +0530

    added route file

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import Header from '../view/component/Header'
+import LandingPage from '../view/page/LandingPage'
+
+export const Routes = () => {
+    <BrowserRouter>
+        <Switch>
+            <Route path={'/header'} component={Header} />
+            <Route component={LandingPage} />
+        </Switch>
+    </BrowserRouter>
+}
+
+export default Routes
