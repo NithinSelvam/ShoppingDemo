9b94a06b55ac805d97b0bd895f53da318568ec8c
Nithin <nithselv@gmail.com>
2021-01-06 18:29:20 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import LandingPage from '../src/view/page/LandingPage';
 import './App.css';
+import Header from './view/component/Header';
 
-function App() {
-  return (
-    <LandingPage />
-  );
+class App extends React.Component {
+  render() {
+    return (<React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path={'/header'} component={Header} />
+          <Route component={LandingPage} />
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>)
+  }
 }
 
 export default App;
