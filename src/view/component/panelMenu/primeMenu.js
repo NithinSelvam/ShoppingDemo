ece2d58d6c2904fe062b79d0523e9f1d1dc9305a
arungowda-p <33170786+arungowda-p@users.noreply.github.com>
2021-01-12 16:17:24 +0530

    panelMenu

---
+++ src/view/component/panelMenu/primeMenu.js
@@ -0,0 +1,137 @@
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import './primeMenu.less';
+
+import React from 'react';
+import { PanelMenu } from 'primereact/panelmenu';
+
+export const PrimeMenu = () => {
+    const items = [
+        {
+            label:'File',
+            icon:'pi pi-fw pi-file',
+            items:[
+                {
+                    label:'New',
+                    icon:'pi pi-fw pi-plus',
+                    items:[
+                    {
+                        label:'Bookmark',
+                        icon:'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label:'Video',
+                        icon:'pi pi-fw pi-video'
+                    }
+                    ]
+                },
+                {
+                    label:'Delete',
+                    icon:'pi pi-fw pi-trash'
+                },
+                {
+                    label:'Export',
+                    icon:'pi pi-fw pi-external-link'
+                }
+            ]
+        },
+        {
+            label:'Edit',
+            icon:'pi pi-fw pi-pencil',
+            items:[
+                {
+                    label:'Left',
+                    icon:'pi pi-fw pi-align-left'
+                },
+                {
+                    label:'Right',
+                    icon:'pi pi-fw pi-align-right'
+                },
+                {
+                    label:'Center',
+                    icon:'pi pi-fw pi-align-center'
+                },
+                {
+                    label:'Justify',
+                    icon:'pi pi-fw pi-align-justify'
+                }
+            ]
+        },
+        {
+            label:'Users',
+            icon:'pi pi-fw pi-user',
+            items:[
+                {
+                    label:'New',
+                    icon:'pi pi-fw pi-user-plus'
+                },
+                {
+                    label:'Delete',
+                    icon:'pi pi-fw pi-user-minus'
+                },
+                {
+                    label:'Search',
+                    icon:'pi pi-fw pi-users',
+                    items:[
+                    {
+                        label:'Filter',
+                        icon:'pi pi-fw pi-filter',
+                        items:[
+                            {
+                                label:'Print',
+                                icon:'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        icon:'pi pi-fw pi-bars',
+                        label:'List'
+                    }
+                    ]
+                }
+            ]
+        },
+        {
+            label:'Events',
+            icon:'pi pi-fw pi-calendar',
+            items:[
+                {
+                    label:'Edit',
+                    icon:'pi pi-fw pi-pencil',
+                    items:[
+                    {
+                        label:'Save',
+                        icon:'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label:'Delete',
+                        icon:'pi pi-fw pi-calendar-minus'
+                    }
+                    ]
+                },
+                {
+                    label:'Archieve',
+                    icon:'pi pi-fw pi-calendar-times',
+                    items:[
+                    {
+                        label:'Remove',
+                        icon:'pi pi-fw pi-calendar-minus'
+                    }
+                    ]
+                }
+            ]
+        }
+    ];
+
+    return (
+        <div>
+            <div className="card">
+                <PanelMenu model={items} style={{ width: '22rem' }}/>
+            </div>
+        </div>
+    );
+}
+
+export default PrimeMenu
--- src/view/page/Home.js
+++ src/view/page/Home.js
@@ -4,133 +4,18 @@ import { addToCart } from '../../model/actions/CartActions'
 import styles from '../Styles.less'
 import { Card } from 'primereact/card'
 import { Button } from 'primereact/button'
-import { PanelMenu  } from 'primereact/panelmenu'
+import { PrimeMenu } from '../component/panelMenu/primeMenu'
 
 export const Home = (props) => {
 
     const handleClick = (id) => {
         props.addToCart(id);
     }
-    const footer = id =>
+    const footer = id => {
         <span>
             <Button label="Add to cart" icon="pi pi-times" className="p-button-secondary p-ml-2" onClick={() => handleClick(id)} />
         </span>
-    let items = [
-        {
-            label:'File',
-            icon:'pi pi-fw pi-file',
-            items:[
-                {
-                    label:'New',
-                    icon:'pi pi-fw pi-plus',
-                    items:[
-                    {
-                        label:'Bookmark',
-                        icon:'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label:'Video',
-                        icon:'pi pi-fw pi-video'
-                    }
-                    ]
-                },
-                {
-                    label:'Delete',
-                    icon:'pi pi-fw pi-trash'
-                },
-                {
-                    label:'Export',
-                    icon:'pi pi-fw pi-external-link'
-                }
-            ]
-        },
-        {
-            label:'Edit',
-            icon:'pi pi-fw pi-pencil',
-            items:[
-                {
-                    label:'Left',
-                    icon:'pi pi-fw pi-align-left'
-                },
-                {
-                    label:'Right',
-                    icon:'pi pi-fw pi-align-right'
-                },
-                {
-                    label:'Center',
-                    icon:'pi pi-fw pi-align-center'
-                },
-                {
-                    label:'Justify',
-                    icon:'pi pi-fw pi-align-justify'
-                }
-            ]
-        },
-        {
-            label:'Users',
-            icon:'pi pi-fw pi-user',
-            items:[
-                {
-                    label:'New',
-                    icon:'pi pi-fw pi-user-plus'
-                },
-                {
-                    label:'Delete',
-                    icon:'pi pi-fw pi-user-minus'
-                },
-                {
-                    label:'Search',
-                    icon:'pi pi-fw pi-users',
-                    items:[
-                    {
-                        label:'Filter',
-                        icon:'pi pi-fw pi-filter',
-                        items:[
-                            {
-                                label:'Print',
-                                icon:'pi pi-fw pi-print'
-                            }
-                        ]
-                    },
-                    {
-                        icon:'pi pi-fw pi-bars',
-                        label:'List'
-                    }
-                    ]
-                }
-            ]
-        },
-        {
-            label:'Events',
-            icon:'pi pi-fw pi-calendar',
-            items:[
-                {
-                    label:'Edit',
-                    icon:'pi pi-fw pi-pencil',
-                    items:[
-                    {
-                        label:'Save',
-                        icon:'pi pi-fw pi-calendar-plus'
-                    },
-                    {
-                        label:'Delete',
-                        icon:'pi pi-fw pi-calendar-minus'
-                    }
-                    ]
-                },
-                {
-                    label:'Archieve',
-                    icon:'pi pi-fw pi-calendar-times',
-                    items:[
-                    {
-                        label:'Remove',
-                        icon:'pi pi-fw pi-calendar-minus'
-                    }
-                    ]
-                }
-            ]
-        }
-    ];
+    }
 
     let itemList = props.items.map(item => {
         return (
@@ -149,11 +34,7 @@ export const Home = (props) => {
     return (
 
         <div className={styles.bodyContainer}>
-            <div className={styles.sideMenu}>
-            <div className="card">
-                <PanelMenu model={items} style={{ width: '22rem' }}/>
-            </div>
-            </div>
+            <PrimeMenu/>
             <div>
                 <h3 className={styles.center}>Our items:</h3>
                 <div className={styles.box}>
